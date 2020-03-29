3a793f4224f991ab59825f6bb5ccee000b8e04d1
Rustem Mustafin <mustafin.rustem@gmail.com>
2020-03-30 02:05:16 +0300

    Disabled postcss for now

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   plugins: [
     "tailwindcss",
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: [
-              "./pages/**/*.{js,jsx,ts,tsx}",
-              "./components/**/*.{js,jsx,ts,tsx}"
-            ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-          }
-        ]
-      : undefined,
+    // [
+    //   "@fullhuman/postcss-purgecss",
+    //   {
+    //     content: [
+    //       "./pages/**/*.{js,jsx,ts,tsx}",
+    //       "./components/**/*.{js,jsx,ts,tsx}"
+    //     ],
+    //     whitelistPatterns: [/bg-.*?-400/g],
+    //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    //   }
+    // ],
     "postcss-preset-env"
   ]
 };

6a60f7d6d7190087fb7482ac5802f4222be4d4f7
Rustem Mustafin <mustafin.rustem@gmail.com>
2020-03-30 02:28:13 +0300

    Ok, this might be a fix

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const bg = {
+  red: "bg-red-400",
+  green: "bg-green-400",
+  blue: "bg-blue-400"
+};
+
 export default ({ icon, header, className, circleColor = "red", children }) => (
   <div className={`${className} w-full md:w-4/12 px-4 text-center`}>
     <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
       <div className="px-4 py-5 flex-auto">
         <div
-          className={`bg-${circleColor}-400 text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full`}
+          className={`${
+            bg[circleColor]
+          } text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full`}
         >
           <FontAwesomeIcon icon={icon} />
         </div>
--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,14 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  }
+];
 module.exports = {
   plugins: [
     "tailwindcss",
-    // [
-    //   "@fullhuman/postcss-purgecss",
-    //   {
-    //     content: [
-    //       "./pages/**/*.{js,jsx,ts,tsx}",
-    //       "./components/**/*.{js,jsx,ts,tsx}"
-    //     ],
-    //     whitelistPatterns: [/bg-.*?-400/g],
-    //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    //   }
-    // ],
-    "postcss-preset-env"
+    "postcss-preset-env",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };

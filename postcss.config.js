e945fa818f37ed9fa4ce91b9cc96e123f3d3a071
Rustem Mustafin <mustafin.rustem@gmail.com>
2020-03-30 01:38:24 +0300

    Trying to fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  }
-];
 module.exports = {
   plugins: [
     "tailwindcss",
-    "postcss-preset-env",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    process.env.NODE_ENV === "production"
+      ? [
+          "@fullhuman/postcss-purgecss",
+          {
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}"
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        ]
+      : undefined,
+    "postcss-preset-env"
   ]
 };

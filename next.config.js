47a6b64bf26288c584a62bca36e939b82c50fd13
Rustem Mustafin <mustafin.rustem@gmail.com>
2020-04-20 04:12:50 +0300

    Add WebP image as bg (#4)
    * Add WebP image as bg
    
    * Delete hb2.jpg

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      /* config for next-optimized-images */
+      optimizeImagesInDev: true
+    }
+  ]
+
+  // your other plugins here
+]);
